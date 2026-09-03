cda78f8452ac1fa0c0e0c3fb422afda4d4c61a97
Taylan Dirican <ertaylan680@gmail.com>
2026-09-03 22:42:33 +0300

    Notes on NumPy array methods have been added.

---
+++ numpy-arrays.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+# result = np.array([1, 3, 5, 7, 9])
+
+# result =np.arange(1,10)     # 1'den 10'a kadar sayılar
+
+# result = np.arange(10, 100, 3)  # 10'dan 100'e kadar 3'er 3'er sayılar
+
+# result = np.zeros(10)   #10 tane 0'dan oluşan dizi
+
+# result = np.ones(10)  # 10 tane 1'den oluşan dizi, her bir eleman float olarak gelir
+
+# result = np.linspace(
+#     0, 100, 5
+# )  # Belirtilen aralıkta eşit aralıklı sayılar üretir. 0 ile 100 arasında 5 tane sayı üretir.
+
+# result = np.linspace(
+#     0, 5, 5
+# )  # Belirtilen aralıkta eşit aralıklı sayılar üretir. 0 ile 5 arasında 20 tane sayı üretir.
+
+# result = np.random.randint(0, 10)   #0 dahil, 10 hariç
+
+# result = np.random.randint(0, 10, 3)  # 0 dahil, 10 hariç, 3 tane sayı üretir
+
+# result = np.random.rand(5)  # 0 ile 1 arasında 5 tane sayı üretir
+# result = np.random.randn(5)  # 0 ile 1 arasında 5 tane sayı üretir, negatif ve pozitif değerler üretir
+
+# np_array = np.arange(0, 50)
+# np_multi = np_array.reshape(5, 10)  # 5 satır ve 10 sütundan oluşan bir dizi oluşturur
+# print(np_multi.sum(axis=1))  # satırların toplamını verir
+# print(np_multi.sum(axis=0))  # sütunların toplamını verir
+
+# rnd_numbers = np.random.randint(1, 100, 10)
+# print(rnd_numbers)
+# result = rnd_numbers.max()  # dizideki en büyük sayıyı verir
+# result = rnd_numbers.min()  # dizideki en küçük sayıyı verir
+# result = rnd_numbers.mean()  # dizideki sayıların ortalamasını verir
+# result = rnd_numbers.argmax()  # dizideki en büyük sayının indeksini verir
+# result = rnd_numbers.argmin()  # dizideki en küçük sayının indeksini verir
+
+
+# print(result)
